fec527ca8ee41ca9841944d9f0313ded1638f6b1
zhuSeaSwallow <1132336834@qq.com>
2018-07-24 22:45:16 +0800

    test github

---
+++ SaveCustomer.java
@@ -0,0 +1,21 @@
+import java.lang.String;
+import java.lang.Thread;
+import java.util.function.IntBinaryOperator;
+
+/**
+ * Created by fangfang on 2018/5/28.
+ */
+public class SaveCustomer {
+    private int zff;
+    public static void main(String[] args) {
+        //String str = "abc";
+        String str = "123";
+
+        for (int a = 0; a < 10; a++) {
+
+
+        }
+    }
+}
+
+
---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Created by fangfang on 2018/6/24.
+ */
+public class Test {
+    int s = 123;
+    String str = "zff";
+    List features = Arrays.asList("Lambdas","Default Method","Stream API","Date and Time");
+    Predicate
+}
